525d1a635743e7c594c397bc00e20ed3d33d76de
tpisfil <tpisfil13@gmail.com>
2022-03-21 19:49:19 -0400

    E1

--- 1twoNumberSum.js
+++ 1twoNumberSum.js
@@ -3,8 +3,8 @@
 //=============================================================================
 //-------------------------------- PROMPT -------------------------------------
 //=============================================================================
-// Write a function that takes in a non-empty array of distinct integers and an
-// integer representing a target sum. If any two numbers in the input array sum
+// Write a function that takes in a nonepresenting a target sum. If-empty array of distinct integers and an
+// integer r any two numbers in the input array sum
 // up to the target sum, the function should return them in an array, in any
 // order. If no two numbers sum up to the target sum, the function should return
 // an empty array.
--- 2validateSubsequence.js
+++ 2validateSubsequence.js
@@ -1,31 +1,62 @@
-function isValidSubsequence(array, seq) {
-    // Write your code here.
-    // for (let i=0; i<sequence.length; i++){
-    //     if (array.includes(sequence[i])){
-    //         console.log("next");
-    //         var idx = array.indexOf(sequence[i]);
-    //         array.splice(idx,1);
-    //     } else {
-    //         return false; 
-    //     }
-    // }
-    // return true;
+//Thomas Pisfil | 03/21/2022
+
+//=============================================================================
+//-------------------------------- PROMPT -------------------------------------
+//=============================================================================
+// Given two non-empty arrays of integers, write a function that determines
+// whether the second array is a subsequence of the first one.
+
+// A subsequence of an array is a set of numbers that aren't necessarily adjacent
+// in the array but that are in the same order as they appear in the array. For
+// instance, the numbers <span>[1, 3, 4]</span> form a subsequence of the array
+// <span>[1, 2, 3, 4]</span>, and so do the numbers <span>[2, 4]</span>. Note
+// that a single number in an array and the array itself are both valid
+// subsequences of the array.
+
+//=============================================================================
+//---------------------------- MY SOLUTION ------------------------------------
+//=============================================================================
+function isValidSubsequence(array, sequence) { //should return a boolean 
+    //is seq in the array? how would we check that?
+    //we could check if each number in from seq is in the array??? 
+    //but it would have to be in order 
 
     let j = 0;
-    for (let i=0; i<array.length; i++){
-        if (array[i] == seq[j]){
-            console.log("hello " + array[i]);
-            j += 1;
-        } 
-    }
-    return j == seq.length;
 
+    for (let i=0; i<array.length; i++){ //iterate through every value in array 
+        if (array[i]==sequence[j]){ //checks if it matches with the current value of j
+            j += 1; //if it does, move to the next value of j 
+        }
+    }
+    return j==sequence.length; //this would return true if it goes through each of the sequences values 
+    //if the last number of seq has an index value of [length-1],
+    //then once it gets to that 1 gets added to j bc it goes in the if statement 
+    //that would set j equal to the length of the sequence 
 }
 
-// array = [5,1,22,25,6,-1,8,10];
-// seq = [1,6,-1,10];
 
-array= [5, 1, 22, 25, 6, -1, 8, 10],
-seq= [5, 1, 22, 22, 25, 6, -1, 8, 10]
+//=============================================================================
+//----------------------------- SOLUTION --------------------------------------
+//=============================================================================
+//pretty much the same thing as mine which means Im a beast :) 
+//O(n) time | O(1) space 
+function isValidSubsequence (array, sequence) {
+    let seqIdx = 0;
 
-console.log(isValidSubsequence(array, seq));
+    for (const value of array){ //go through every number in array 
+        if(seqIdx === sequence.length){
+            return true; //it will get here if it finds the sequence before finishing the array
+        }
+        if (sequence[seqIdx] === value){
+            seqIdx++; //if it matches then point to the next number in seq to find in array 
+        }
+    }
+    return seqIdx === sequence.length;
+}
+//=============================================================================
+//----------------------------- TEST CASES ------------------------------------
+//=============================================================================
+array = [5, 1, 22, 25, 6, -1, 8, 10];
+sequence = [1, 6, -1, 10];
+
+console.log();
