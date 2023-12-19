137749d012b1e98a1b1134763fb7a41c200d51d2
tpisfil <tpisfil13@gmail.com>
2023-12-19 17:35:43 -0500

    updated

--- leetcode/58-lengthOfLastWord.js
+++ leetcode/58-lengthOfLastWord.js
@@ -42,4 +42,4 @@ var lengthOfLastWord = function(s) {
 // Constraints:
 // 1 <= s.length <= 104
 // s consists of only English letters and spaces ' '.
-// There will be at least one word in s.
+// There will be at least one word in s. 
